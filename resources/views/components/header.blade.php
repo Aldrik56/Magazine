88de92090939c9f47e3800e192abc1104eeb4e6d
Aldrik56 <aldrik.ciaputra@student.umn.ac.id>
2023-08-08 18:41:39 +0700

    Revert "hapus vite"
    This reverts commit ff2ebde9517da6708f9941ada6864df2e9889fbc.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,6 @@
+@vite(['resources/css/app.css', 'resources/js/app.js', 
+        'resources/sass/styles.scss','resources/sass/home.scss',
+        'resources/sass/loading.scss','resources/sass/deskripsi.scss'])
 <head>
     <meta charset="UTF-8">
     <title>ULTIMAGZ</title>
