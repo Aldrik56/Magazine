b0ec4a32635793a4971d2de4f025ffad901e007d
Gynthunder <maryadigt@gmail.com>
2023-08-20 19:56:10 +0700

    footer dan desc
    tes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,6 @@
 {{-- ini footer (dikerjain maryadi) --}}
 <footer>
 <div class="footer">
-    <div class="parent">
     <div class="desc-judul">
         <h3>About Us</h3>
         <p>Ultimagz merupakan sebuah majalah kampus independen yang berlokasi di Universitas Multimedia Nusantara (UMN). Ultimagz pertama kali terbit pada tahun 2007. Saat itu, keluarga Ultimagz generasi pertama berhasil menerbitkan sebuah majalah yang bertujuan membantu mempromosikan kampus. Ultimagz saat itu juga menjadi wadah pelatihan menulis bagi mahasiswa Fakultas Ilmu Komunikasi (FIKOM) UMN dan non-FIKOM.</p>
@@ -13,6 +12,5 @@
         <img src="instagram.svg" width="50px" height="40px">
         <img src="youtube.svg" width="50px" height="40px">
     </div>
-</div>
 </div>
 </footer>
--- resources/views/pages/deskripsi.blade.php
+++ resources/views/pages/deskripsi.blade.php
@@ -14,26 +14,66 @@
     <img class="pdf" src="test 5.svg">
     <div class="parent-title">
     <h1>Title : Lorem Ipsum Dolor Sit Amet</h1>
+    <br>
     <p>Edisi : 2021</p>
+    <br>
     <p>Terbit : Januari 4, 2023</p>
+    <br>
     <p>Tebal : 89 halaman</p>
+    <form action="deskripsi" method="post" class='hero__buttons'>
+                <button type="submit" class='read'>
+                    <strong> <a>Read Now</a></strong>
+                </button>
+                <button type="submit" class="desc">
+                    <strong><a href='#'>Share</a></strong>
+                </button>
+            </form>
     </div>
 </div>
 <div class="parent-deskripsi">
     <h3>Description :</h3>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+    <br>
     <h4>Bahasa :</h4>
     <p>Bahasa Indonesia</p>
-    <form action="deskripsi" method="post" class='hero__buttons'>
-                <button type="submit" class='read'>
-                    <strong> <a href='/pdf/{{$magazines[count($magazines)-1]->id}}'>Read Now</a></strong>
-                </button>
-                <button type="submit" class="desc">
-                    <strong><a href='#'>Description</a></strong>
-                </button>
-            </form>
-</div>
     
+</div>
     @include('components.footer')
+    <script>
+        //ini promise function dari pdf js
+        async function renderIllustration(url, index) {
+            const canvas = document.querySelector("#highlight_magazine");
+            const magazineUrl = '{{ asset('storage/') }}/' + url;
+    
+            console.log(magazineUrl);
+            var loadingTask = pdfjsLib.getDocument(magazineUrl);
+            var scale = 1;
+            const context = canvas.getContext('2d');
+            
+            const pdf = await loadingTask.promise;
+            const page = await pdf.getPage(1);
+            
+            const viewport = page.getViewport({ scale: scale });
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+            
+            const renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            
+            await page.render(renderContext).promise;
+        }
+        
+        renderIllustration( listMagazine[listMagazine.length -1].file,  1);
+    
+        
+    </script>
+    <script src="{{ URL::asset('libraryJs/pdf.js') }}" type="text/javascript"></script>
+    <script src="{{ URL::asset('libraryJs/pdf.worker.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src='./js/navbar.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-element-bundle.min.js"></script>
+    {{-- <script src='./js/swiper.js'></script> --}}
 </body>
 </html>
